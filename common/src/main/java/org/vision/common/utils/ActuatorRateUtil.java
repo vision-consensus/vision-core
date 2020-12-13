41cfce17da0eedf60a2d81031ff305b10638f17e
Jerry <jerrygavin2020@gmail.com>
2020-12-13 12:26:42 +0800

    reward test

--- common/src/main/java/org/vision/common/utils/ActuatorRateUtil.java
+++ common/src/main/java/org/vision/common/utils/ActuatorRateUtil.java
@@ -3,9 +3,9 @@ package org.vision.common.utils;
 public class ActuatorRateUtil {
     public static long getActuatorRate(long headNumber) {
         long rate = 0;
-        if(headNumber >=0 && headNumber < 2591280){
+        if(headNumber >=0 && headNumber < 1000){
             rate = 3;
-        }else if(headNumber >=2591280 && headNumber < 5182560){
+        }else if(headNumber >=1000 && headNumber < 3000){
             rate = 2;
         }else{
             rate = 1;
