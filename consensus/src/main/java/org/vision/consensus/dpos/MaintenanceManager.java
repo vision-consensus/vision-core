f8e8d8550bac588b7c5b56cc3ed4542cfd700a95
byron <newjava1sina>
2021-07-01 22:57:39 +0800

    judge SRGuaranteeFrozenBalance>srFreezeLowest

--- consensus/src/main/java/org/vision/consensus/dpos/MaintenanceManager.java
+++ consensus/src/main/java/org/vision/consensus/dpos/MaintenanceManager.java
@@ -126,9 +126,11 @@ public class MaintenanceManager {
         }
 
         DynamicPropertiesStore dynamicPropertiesStore = consensusDelegate.getDynamicPropertiesStore();
-        long maxVoteCounts = (long) ((account.getSRGuaranteeFrozenBalance() - dynamicPropertiesStore.getSrFreezeLowest())
-                /(Double.parseDouble(dynamicPropertiesStore.getSrFreezeLowestPercent()+"") / Parameter.ChainConstant.SR_FREEZE_LOWEST_PRECISION));
-
+        long maxVoteCounts = 0;
+        if (account.getSRGuaranteeFrozenBalance() > dynamicPropertiesStore.getSrFreezeLowest()) {
+          maxVoteCounts = (long) ((account.getSRGuaranteeFrozenBalance() - dynamicPropertiesStore.getSrFreezeLowest())
+                  /(Double.parseDouble(dynamicPropertiesStore.getSrFreezeLowestPercent()+"") / Parameter.ChainConstant.SR_FREEZE_LOWEST_PRECISION));
+        }
         witnessCapsule.setVoteCountWeight(witnessCapsule.getVoteCountWeight() + voteBuilder.getVoteCountWeight());
         witnessCapsule.setVoteCount(witnessCapsule.getVoteCount() + voteBuilder.getVoteCount());
         witnessCapsule.setVoteCountThreshold(maxVoteCounts);
