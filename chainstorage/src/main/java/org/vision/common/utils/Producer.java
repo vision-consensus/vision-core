9811351d52c1f09ecc7f358652ea2ceda9c3e97a
Jerry <JerryGavin2020@gmail.com>
2021-07-05 09:27:22 +0800

    fix kafka producer error

---
+++ chainstorage/src/main/java/org/vision/common/utils/Producer.java
@@ -0,0 +1,72 @@
+package org.vision.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.vision.common.parameter.CommonParameter;
+
+import java.util.Objects;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class Producer {
+    private final KafkaProducer<String, String> producer;
+    public static Producer instance;
+    public static Producer getInstance() {
+        if (Objects.isNull(instance)) {
+            synchronized (Producer.class) {
+                if (Objects.isNull(instance)) {
+                    instance = new Producer();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private Producer() {
+        logger.info("kafka bootstrap servers:"+CommonParameter.PARAMETER.getKafkaBootStrapServers());
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonParameter.PARAMETER.getKafkaBootStrapServers());
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producer = new KafkaProducer<>(props);
+    }
+
+    public void send(String topic, String message){
+        try {
+            producer.send(new ProducerRecord<>(topic,
+                    UUID.randomUUID().toString(),
+                    message)).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ProducerCallBack implements Callback {
+
+    private final long startTime;
+    private final int key;
+    private final String message;
+
+    public ProducerCallBack(long startTime, int key, String message) {
+        this.startTime = startTime;
+        this.key = key;
+        this.message = message;
+    }
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        if (metadata != null) {
+            System.out.println(
+                    "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
+                            "), " +
+                            "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
+        } else {
+            exception.printStackTrace();
+        }
+    }
+}
--- chainstorage/src/main/java/org/vision/core/store/ContractStore.java
+++ chainstorage/src/main/java/org/vision/core/store/ContractStore.java
@@ -1,11 +1,15 @@
 package org.vision.core.store;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Streams;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.vision.common.parameter.CommonParameter;
+import org.vision.common.utils.JsonFormat;
+import org.vision.common.utils.Producer;
 import org.vision.core.capsule.ContractCapsule;
 import org.vision.core.db.VisionStoreWithRevoking;
 import org.vision.protos.contract.SmartContractOuterClass.SmartContract;
