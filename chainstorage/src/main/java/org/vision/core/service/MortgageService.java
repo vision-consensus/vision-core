54b6aa052506b4c893924b868d8fedaa9b20756d
darylcc <chen_eh@163.com>
2021-08-07 01:13:02 +0800

    update getspreadmintparent result

--- chainstorage/src/main/java/org/vision/core/service/MortgageService.java
+++ chainstorage/src/main/java/org/vision/core/service/MortgageService.java
@@ -353,8 +353,8 @@ public class MortgageService {
           break;
         }
 
-        addressList.add(spreadRelationShipCapsule.getOwner().toString());
-        if (addressList.contains(spreadRelationShipCapsule.getParent().toString())){ // deal loop parent address
+        addressList.add(Hex.toHexString(spreadRelationShipCapsule.getOwner().toByteArray()));
+        if (addressList.contains(Hex.toHexString(spreadRelationShipCapsule.getParent().toByteArray()))){ // deal loop parent address
           break;
         }
 
