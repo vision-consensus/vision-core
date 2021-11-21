8806c60eb3b28eaf2f2f5c441c13d13de936c7e3
Jerry <JerryGavin2020@gmail.com>
2021-11-21 23:15:07 +0800

    for merging into vpioneer

--- executor/src/main/java/org/vision/core/actuator/FreezeBalanceActuator.java
+++ executor/src/main/java/org/vision/core/actuator/FreezeBalanceActuator.java
@@ -68,7 +68,7 @@ public class FreezeBalanceActuator extends AbstractActuator {
     long newBalance = accountCapsule.getBalance() - freezeBalanceContract.getFrozenBalance();
 
     long frozenBalance = freezeBalanceContract.getFrozenBalance();
-    long expireTime = now + 180000L;
+    long expireTime = now + duration;
 
     byte[] receiverAddress = freezeBalanceContract.getReceiverAddress().toByteArray();
     byte[] parentAddress = freezeBalanceContract.getParentAddress().toByteArray();
