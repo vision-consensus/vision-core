48067e8f58f29e8109369f7920b6df4a0ad3fb78
Jerry <JerryGavin2020@gmail.com>
2021-11-22 23:35:41 +0800

    WITNESS_STANDBY_LENGTH

--- common/src/main/java/org/vision/core/config/Parameter.java
+++ common/src/main/java/org/vision/core/config/Parameter.java
@@ -40,7 +40,7 @@ public class Parameter {
 
 
     public static final int MAX_ACTIVE_WITNESS_NUM = 5;
-    public static final int WITNESS_STANDBY_LENGTH = 100;
+    public static final int WITNESS_STANDBY_LENGTH = 105;
     public static final long TRANSFER_FEE = 0; // free
     public static final int BLOCK_PRODUCED_INTERVAL = 3000; //ms,produce block period, must be divisible by 60. millisecond
     public static final int MAX_VOTE_NUMBER = 30;
