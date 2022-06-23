2813f55e1ce5650e87f769288a2be4c7629cc248
RhymeWalker <beginningrhyme@gmail.com>
2022-06-23 11:48:24 +0800

    versiob modify

--- framework/src/main/java/org/vision/program/Version.java
+++ framework/src/main/java/org/vision/program/Version.java
@@ -2,9 +2,9 @@ package org.vision.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "Ultima-v1.1.1";
+  public static final String VERSION_NAME = "Ultima-v1.1.2";
   public static final String VERSION_CODE = "10000";
-  private static final String VERSION = "1.1.1";
+  private static final String VERSION = "1.1.2";
 
   public static String getVersion() {
     return VERSION;
