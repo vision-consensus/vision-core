8687a8a9ce74955c56db5e55ffae1935b77d3e0a
Lemon <lemon220326@gmail.com>
2022-06-26 23:40:06 +0800

    fix program version

--- framework/src/main/java/org/vision/program/Version.java
+++ framework/src/main/java/org/vision/program/Version.java
@@ -2,9 +2,9 @@ package org.vision.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "Ultima-v1.1.1";
-  public static final String VERSION_CODE = "10000";
-  private static final String VERSION = "1.1.1";
+  public static final String VERSION_NAME = "Ultima-v1.2.0";
+  public static final String VERSION_CODE = "10001";
+  private static final String VERSION = "1.2.0";
 
   public static String getVersion() {
     return VERSION;
