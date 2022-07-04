1404cfd4cb3af7d9c462786b19d76621abee57cb
daryl <chen_eh@163.com>
2022-07-04 10:55:18 +0800

    add the getAllowEthereumCompatibleTransactionNativeStep1 field to the wallet/getchainparameters api

--- framework/src/main/java/org/vision/core/Wallet.java
+++ framework/src/main/java/org/vision/core/Wallet.java
@@ -1180,6 +1180,10 @@ public class Wallet {
             .setKey("getVPFreezeStageWeight")
             .setStringValue(dbManager.getDynamicPropertiesStore().getVPFreezeStageWeight())
             .build());
+    builder.addChainParameter(Protocol.ChainParameters.ChainParameter.newBuilder()
+            .setKey("getAllowEthereumCompatibleTransactionNativeStep1")
+            .setValue(dbManager.getDynamicPropertiesStore().getAllowEthereumCompatibleTransactionNativeStep1())
+            .build());
     return builder.build();
   }
 
