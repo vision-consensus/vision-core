5cecbbae321449443b0d76ccae874d08cc3d680e
daryl <chen_eh@163.com>
2022-10-26 14:27:47 +0800

    add ACCOUNTFROZENSTAGE topic to mongo branch

--- chainstorage/src/main/java/org/vision/core/store/AccountFrozenStageResourceStore.java
+++ chainstorage/src/main/java/org/vision/core/store/AccountFrozenStageResourceStore.java
@@ -2,9 +2,13 @@ package org.vision.core.store;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
+import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.vision.common.parameter.CommonParameter;
+import org.vision.common.utils.JsonFormat;
+import org.vision.common.utils.Producer;
 import org.vision.core.capsule.AccountFrozenStageResourceCapsule;
 import org.vision.core.db.VisionStoreWithRevoking;
 
@@ -21,5 +25,13 @@ public class AccountFrozenStageResourceStore extends VisionStoreWithRevoking<Acc
   public AccountFrozenStageResourceCapsule get(byte[] key) {
     byte[] value = revokingDB.getUnchecked(key);
     return ArrayUtils.isEmpty(value) ? null : new AccountFrozenStageResourceCapsule(value);
+  }
+
+  @Override
+  public void put(byte[] key, AccountFrozenStageResourceCapsule item) {
+    super.put(key, item);
+    if(CommonParameter.PARAMETER.isKafkaEnable()){
+      Producer.getInstance().send("ACCOUNTFROZENSTAGE", Hex.toHexString(item.getInstance().getAddress().toByteArray()), JsonFormat.printToString(item.getInstance()));
+    }
   }
 }
